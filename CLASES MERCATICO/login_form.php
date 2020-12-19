3bc2ac601cdbd704d4d3bbc4cf428cc3ac143b3c
YendryMora <45083159+YendryMora@users.noreply.github.com>
2020-12-19 05:33:58 -0600

    Add files via upload

---
+++ CLASES MERCATICO/login_form.php
@@ -0,0 +1,101 @@
+<?php
+#this is Login form page , if user is already logged in then we will not allow user to access this page by executing isset($_SESSION["uid"])
+#if below statment return true then we will send user to their profile.php page
+if (isset($_SESSION["uid"])) {
+	header("location:profile.php");
+}
+//in action.php page if user click on "ready to checkout" button that time we will pass data in a form from action.php page
+if (isset($_POST["login_user_with_product"])) {
+	//this is product list array
+	$product_list = $_POST["product_id"];
+	//here we are converting array into json format because array cannot be store in cookie
+	$json_e = json_encode($product_list);
+	//here we are creating cookie and name of cookie is product_list
+	setcookie("product_list",$json_e,strtotime("+1 day"),"/","","",TRUE);
+
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>MERCATICO</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top" id="nabsito">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<a href="index.php" class="navbar-brand"><img src = "product_images/MERCA.png" id="logo_cart" width="100px" height="28px"></a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
+			</ul>
+		</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="signup_msg">
+				<!--Alert from signup form-->
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Iniciar Sesion</div>
+					<div class="panel-body">
+						<!--User Login Form-->
+						<form onsubmit="return false" id="login">
+							<label for="email">Email</label>
+							<input type="email" class="form-control" name="email" id="email" required/>
+							<label for="email">Contraseña</label>
+							<input type="password" class="form-control" name="password" id="password" required/>
+							<p><br/></p>
+							<input type="submit" class="btn btn-success" style="float:right;" Value="Ingresar">
+							<!--If user dont have an account then he/she will click on create account button-->
+							<div><a href="customer_registration.php?register=1">Crear cuenta</a></div>						
+						</form>
+				</div>
+				<div class="panel-footer"><div id="e_msg"></div></div>
+			</div>
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
